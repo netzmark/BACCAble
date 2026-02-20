5326eb81eec412f5925c994bf9c5c38b2db915bb
netzmark <netzmark@DESKTOP-S50DNLL>
2026-02-20 15:03:49 +0100

    Added ports' support needed for lowConsume capabilities on UCAN boards.

--- firmware/ledsStripController/Core/Inc/lowConsume.h
+++ firmware/ledsStripController/Core/Inc/lowConsume.h
@@ -12,6 +12,16 @@
 	#define CAN_LOW_CONSUME_Port GPIOA
 	#define CAN_LOW_CONSUME CAN_LOW_CONSUME_Port , CAN_LOW_CONSUME_Pin
 
+	/* Ports for UCAN boards */
+	#define CHIP_LOW_CONSUME2_Pin GPIO_PIN_13
+	#define CHIP_LOW_CONSUME2_Port GPIOA
+	#define CHIP_LOW_CONSUME2 CHIP_LOW_CONSUME2_Port , CHIP_LOW_CONSUME2_Pin
+
+	#define CAN_LOW_CONSUME2_Pin GPIO_PIN_13
+	#define CAN_LOW_CONSUME2_Port GPIOC
+	#define CAN_LOW_CONSUME2 CAN_LOW_CONSUME2_Port , CAN_LOW_CONSUME2_Pin
+	//
+
 	#define CHIP_RESET_DURATION 500 //time in milliseconds
 
 	void lowConsume_init(void);
